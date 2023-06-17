05cc3576d639491b080c6f64f29b65aa29c5be57
snydernb1 <snydernb1@gmail.com>
2023-06-17 12:17:25 -0600

    Fixed typo in migration file

--- migrations/versions/20230616_094349_.py
+++ migrations/versions/20230616_094349_.py
@@ -83,7 +83,7 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     if environment == "production":
-        op.execute(f"ALTER TABLE users carts SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE carts SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
