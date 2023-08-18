683427fac57a619c060b244ff6e3e5a88aa4688c
snydernb1 <snydernb1@gmail.com>
2023-08-18 11:31:28 -0600

    Started creating a loading bar for initial fetch from backend

--- react-app/src/components/AllListings/index.js
+++ react-app/src/components/AllListings/index.js
@@ -4,35 +4,66 @@ import { useDispatch, useSelector } from "react-redux";
 
 import ListingCard from "./ListingCard";
 
+import greyLeaf from '../imgs/grey_leaf.png'
+import greenLeaf from '../imgs/green_leaf.png'
+
 import './Listing.css'
+import { useEffect, useState } from "react";
 
 export default function AllListings(){
     const listingsObj = useSelector(state => state.listings.listings)
     const sessionUser = useSelector(state => state.session.user);
+    const [showListings, setShowListings] = useState(false)
     const listings = Object.values(listingsObj)
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setShowListings(true)
+    //     }, '3500')
+
+    // }, [])
 
 
     return (
         <section className="allListingContainer">
+
             <div className="banner">
 
             </div>
+
                 {sessionUser &&
                 <div id="welcomeMessage">
+                    <p className="welcomeMessage">Welcome back,</p>
 
-                <p className="welcomeMessage">Welcome back,</p>
-
-                <p className="welcomeMessage" id="name"> {sessionUser.first_name}!</p>
+                    <p className="welcomeMessage" id="name"> {sessionUser.first_name}!</p>
                 </div>
                 }
-            <div className="listings">
+
+            {showListings === false &&
+
+                <div id="loadingLeaf">
+                    <i className="fas fa-seedling" id="colorChangeLeaf"/>
+                    {/* <img src={greyLeaf} id="greyLeaf"/>
+                    <img src={greenLeaf} id="greenLeaf"/> */}
+
+                </div>
+
+            }
+
+
+            {showListings &&
+
+                <div className="listings">
                 {listings.map((listing) => (
                     <ListingCard
                     key={listing.id}
                     listing={listing}
                     />
-                ))}
-            </div>
+                    ))}
+                </div>
+            }
+
+
         </section>
     );
 };
