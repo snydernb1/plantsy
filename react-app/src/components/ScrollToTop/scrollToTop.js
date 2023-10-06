46b58036265171e2401033f7c6e0a6874c04cbe3
snydernb1 <snydernb1@gmail.com>
2023-10-06 10:01:50 -0600

    Added scroll to top functionality on page change

---
+++ react-app/src/components/ScrollToTop/scrollToTop.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+    const { pathname } = useLocation();
+    console.log('pathName', pathname)
+
+
+    useEffect(() => {
+
+        window.scrollTo(0, 0);
+
+    }, [pathname]);
+
+
+    return null
+}
